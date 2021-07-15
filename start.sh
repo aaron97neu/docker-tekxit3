e906206c7bef59b145feb997a4c26f1996244199
Aaron Neustedter <aneusted@purdue.edu>
2021-07-14 21:06:37 -0500

    Typo

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ wget -q ${URL} -O /tekxit/tekxit.zip
 
 # The excluded files will likely not be found, but let's be safe
 unzip -u /tekxit/tekxit.zip -x "*server.properties" "*ops.json" "*banned-ips.json" "*banned-players.json" "*whitelist.json" "*usercache.json" -d /tekxit
-cp -a /tekxit/${VERSION}/. /tekxit. && rm -fr /tekxit${VERSION}/
+cp -a /tekxit/${VERSION}/. /tekxit && rm -fr /tekxit${VERSION}/
 
 chmod +x /tekxit/ServerLinux.sh
 /tekxit/ServerLinux.sh
