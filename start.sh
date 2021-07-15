2f7094c27425fb6c0e254b2b11511bee66471e7b
Aaron Neustedter <aneusted@purdue.edu>
2021-07-14 21:19:50 -0500

    cd

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ URL=https://www.tekx.it/downloads/${VERSION}.zip
 
 mkdir -p /tekxit
 
+cd tekxit 
 wget -q ${URL} -O /tekxit/tekxit.zip
 
 # The excluded files will likely not be found, but let's be safe
 unzip -u /tekxit/tekxit.zip -x "*server.properties" "*ops.json" "*banned-ips.json" "*banned-players.json" "*whitelist.json" "*usercache.json" -d /tekxit
 cp -a /tekxit/${VERSION}/. /tekxit && rm -fr /tekxit${VERSION}/
 
+ls
 chmod +x /tekxit/ServerLinux.sh
 /tekxit/ServerLinux.sh
